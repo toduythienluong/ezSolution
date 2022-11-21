46b1b2670789f88cf4161bbb19f390d9e6e101f6
luong.to@kms-solutions.asia <luong.to@kms-solutions.asia>
2022-11-21 23:33:35 +0700

    refactor

--- ezExperiment/EZT.API/Controllers/TaxReturnController.cs
+++ ezExperiment/EZT.API/Controllers/TaxReturnController.cs
@@ -20,7 +20,8 @@ public class TaxReturnController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetTaxReturns")]
+    [HttpGet]
+    [Route("GetTaxReturns")]
     public IEnumerable<string> GetTaxReturns()
     {
         return new List<string>() {
@@ -30,7 +31,8 @@ public class TaxReturnController : ControllerBase
         };
     }
 
-    [HttpPost(Name = "CalculateDataRecord")]
+    [HttpPost]
+    [Route("CalculateDataRecord")]
     public string CalculateDataRecord([FromBody] CalculateDataRecordRequest request)
     {
         //TODO this is just for demo
@@ -72,7 +74,7 @@ public class TaxReturnController : ControllerBase
         if (!"string".Equals(postString))
             return JsonSerializer.Deserialize<DataRecord>(postString);
 
-        var mock = "{\"DataRecordId\":\"102340\",\"RecordName\":\"federal\",\"Sections\":[{\"SectionName\":\"personalinformation\",\"DataFields\":[{\"FieldName\":\"firstName\",\"FieldValue\":\"Adam\",\"FieldType\":\"input\",\"DataType\":\"string\"},{\"FieldName\":\"lastName\",\"FieldValue\":\"Smith\",\"FieldType\":\"input\",\"DataType\":\"string\"},{\"FieldName\":\"fullName\",\"FieldValue\":\"\",\"FieldType\":\"calculated\",\"DataType\":\"string\",\"CalculationExpression\":\"'{{federal.personalinformation.firstName}}' + ' ' + '{{federal.personalinformation.lastName}}'\",\"ExpressionToExecute\":\"\"}]},{\"SectionName\":\"income\",\"DataFields\":[{\"FieldName\":\"net\",\"FieldValue\":\"1100\",\"FieldType\":\"double\",\"DataType\":\"int\"},{\"FieldName\":\"tax\",\"FieldValue\":\"\",\"FieldType\":\"calculated\",\"DataType\":\"double\",\"CalculationExpression\":\"{{federal.income.net}} * 0.1\",\"ExpressionToExecute\":\"\"}]}]}";
+        var mock = "{\"DataRecordId\":\"102340\",\"RecordName\":\"federal\",\"Sections\":[{\"SectionName\":\"personalinformation\",\"DataFields\":[{\"FieldName\":\"firstName\",\"FieldValue\":\"Adam\",\"FieldType\":\"input\",\"DataType\":\"string\"},{\"FieldName\":\"lastName\",\"FieldValue\":\"Smith\",\"FieldType\":\"input\",\"DataType\":\"string\"},{\"FieldName\":\"fullName\",\"FieldValue\":\"\",\"FieldType\":\"calculated\",\"DataType\":\"string\",\"CalculationExpression\":\"'{{federal.personalinformation.firstName}}' + ' ' + '{{federal.personalinformation.lastName}}'\",\"ExpressionToExecute\":\"\"}]},{\"SectionName\":\"income\",\"DataFields\":[{\"FieldName\":\"net\",\"FieldValue\":\"1100\",\"FieldType\":\"double\",\"DataType\":\"int\"},{\"FieldName\":\"tax\",\"FieldValue\":\"\",\"FieldType\":\"calculated\",\"DataType\":\"double\",\"CalculationExpression\":\"{{federal.income.net}} * 0.17\",\"ExpressionToExecute\":\"\"}]}]}";
 
         return JsonSerializer.Deserialize<DataRecord>(mock);
     }
